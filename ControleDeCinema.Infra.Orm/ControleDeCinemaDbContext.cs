ed7b52e7d86c31cd15c2c01287ca1a9d1df8526f
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-07-30 18:13:32 -0300

    Implementando projeto base

---
+++ ControleDeCinema.Infra.Orm/ControleDeCinemaDbContext.cs
@@ -0,0 +1,51 @@
+﻿using ControleDeCinema.Dominio.ModuloFilme;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+
+namespace ControleDeCinema.Infra.Orm.Compartilhado;
+
+public class ControleDeCinemaDbContext : DbContext
+{
+    public DbSet<Filme> Filmes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString("SqlServer")!;
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        base.OnConfiguring(optionsBuilder);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Filme>(filmeBuilder =>
+        {
+            filmeBuilder.ToTable("TbFilme");
+
+            filmeBuilder.Property(f => f.Id)
+                .IsRequired()
+                .ValueGenerateOnAdd();
+
+            filmeBuilder.Property(f => f.Titulo)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            filmeBuilder.Property(f => f.Duracao)
+                .IsRequired()
+                .HasColumnType("datetime2");
+
+            filmeBuilder.Property(f => f.Genero)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
