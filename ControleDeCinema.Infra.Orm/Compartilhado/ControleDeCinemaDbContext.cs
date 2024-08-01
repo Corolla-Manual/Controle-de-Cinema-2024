e065b01e865622ddfabdd7bbcdc9e58a191fd64f
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-08-01 14:42:18 -0300

    Criação da Orm para Ingresso e Sessao

--- ControleDeCinema.Infra.Orm/Compartilhado/ControleDeCinemaDbContext.cs
+++ ControleDeCinema.Infra.Orm/Compartilhado/ControleDeCinemaDbContext.cs
@@ -1,76 +1,140 @@
 ﻿using ControleDeCinema.Dominio.ModuloFilme;
 using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Dominio.ModuloSessao;
+using ControleDeCinema.Dominio.ModuloSessao.ModuloIngresso;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ControleDeCinema.Infra.Orm.Compartilhado;
 
 public class ControleDeCinemaDbContext : DbContext
 {
-    public DbSet<Filme> Filmes { get; set; }
-    public DbSet<Sala> Salas { get; set; }
+	public DbSet<Filme> Filmes { get; set; }
+	public DbSet<Sala> Salas { get; set; }
+	public DbSet<Sessao> Sessoes { get; set; }
+	public DbSet<Ingresso> Ingressos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        IConfigurationRoot config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		IConfigurationRoot config = new ConfigurationBuilder()
+			.SetBasePath(Directory.GetCurrentDirectory())
+			.AddJsonFile("appsettings.json")
+			.Build();
 
-        string connectionString = config.GetConnectionString("SqlServer")!;
+		string connectionString = config.GetConnectionString("SqlServer")!;
 
-        optionsBuilder.UseSqlServer(connectionString);
+		optionsBuilder.UseSqlServer(connectionString);
 
-        base.OnConfiguring(optionsBuilder);
-    }
+		base.OnConfiguring(optionsBuilder);
+	}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Filme>(filmeBuilder =>
-        {
-            filmeBuilder.ToTable("TbFilme");
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		//Filme
+		modelBuilder.Entity<Filme>(filmeBuilder =>
+		{
+			filmeBuilder.ToTable("TbFilme");
 
-            filmeBuilder.Property(f => f.Id)
-                .IsRequired()
-                .ValueGenerateOnAdd();
+			filmeBuilder.Property(f => f.Id)
+				.IsRequired()
+				.ValueGeneratedOnAdd();
 
-            filmeBuilder.Property(f => f.Titulo)
-                .IsRequired()
-                .HasColumnType("varchar(200)");
+			filmeBuilder.Property(f => f.Titulo)
+				.IsRequired()
+				.HasColumnType("varchar(200)");
 
-            filmeBuilder.Property(f => f.Duracao)
-                .IsRequired()
-                .HasColumnType("datetime2");
+			filmeBuilder.Property(f => f.Duracao)
+				.IsRequired()
+				.HasColumnType("datetime2");
 
-            filmeBuilder.Property(f => f.Genero)
-                .IsRequired()
-                .HasColumnType("varchar(100)");
+			filmeBuilder.Property(f => f.Genero)
+				.IsRequired()
+				.HasColumnType("varchar(100)");
 
-            filmeBuilder.Property(f => f.Estreia)
-                .IsRequired()
-                .HasColumnType("bit");
-        });
+			filmeBuilder.Property(f => f.Estreia)
+				.IsRequired()
+				.HasColumnType("bit");
+		});
 
-        base.OnModelCreating(modelBuilder);
-    }
+		//Sala
+		modelBuilder.Entity<Sala>(salaBuilder =>
+		{
+			salaBuilder.ToTable("TbSala");
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Sala>(salaBuilder =>
-        {
-            salaBuilder.ToTable("TbSala");
+			salaBuilder.Property(s => s.Id)
+				.IsRequired()
+				.ValueGeneratedOnAdd();
 
-            salaBuilder.Property(s => s.Id)
-                .IsRequired()
-                .ValueGenerateOnAdd();
+			salaBuilder.Property(s => s.Numero)
+				.IsRequired()
+				.HasColumnType("int");
 
-            salaBuilder.Property(s => s.Numero)
-                .IsRequired()
-                .HasColumnType("int");
+			salaBuilder.Property(s => s.Capacidade)
+				.IsRequired()
+				.HasColumnType("int");
+		});
 
-            salaBuilder.Property(s => s.Capacidade)
-                .IsRequired()
-                .HasColumnType("int");
-        });
+		//Sessao
+		modelBuilder.Entity<Sessao>(sessaoBuilder =>
+		{
+			sessaoBuilder.ToTable("TbSessao");
 
-        base.OnModelCreating(modelBuilder);
-    }
+			sessaoBuilder.Property(s => s.Id)
+				.IsRequired()
+				.ValueGeneratedOnAdd();
+
+			sessaoBuilder.Property(s => s.Horario)
+				.IsRequired()
+				.HasColumnType("datetime2");
+
+			sessaoBuilder.Property(s => s.IngressosDisponiveis)
+				.IsRequired()
+				.HasColumnType("int");
+
+			sessaoBuilder.HasOne(s => s.Filme)
+				.WithMany(f => f.Sessoes)
+				.HasForeignKey("Filme_Id")
+				.HasConstraintName("Fk_TbSessao_TbFilme")
+				.OnDelete(DeleteBehavior.Restrict);
+
+			sessaoBuilder.HasOne(s => s.Sala)
+				.WithMany(s => s.Sessoes)
+				.HasForeignKey("Sala_Id")
+				.HasConstraintName("FK_TbSessao_TbSala")
+				.OnDelete(DeleteBehavior.Restrict);
+
+			sessaoBuilder.HasMany(s => s.Ingressos)
+				.WithOne()
+				.HasForeignKey("Sessao_Id")
+				.HasConstraintName("FK_TbSessao_TbIngresso")
+				.OnDelete(DeleteBehavior.Restrict);
+		});
+
+		//Ingresso
+
+		modelBuilder.Entity<Ingresso>(ingressoBuilder =>
+		{
+			ingressoBuilder.ToTable("TbIngresso");
+
+			ingressoBuilder.Property(i => i.Id)
+				.IsRequired()
+				.ValueGeneratedOnAdd();
+
+			ingressoBuilder.Property(i => i.NumeroPoltrona)
+				.IsRequired()
+				.HasColumnType("int");
+
+			ingressoBuilder.Property(i => i.Tipo)
+				.IsRequired()
+				.HasColumnType("int");
+
+			ingressoBuilder.Property(i => i.Valor)
+				.IsRequired()
+				.HasColumnType("real");
+
+
+		});
+
+		base.OnModelCreating(modelBuilder);
+	}
 }
