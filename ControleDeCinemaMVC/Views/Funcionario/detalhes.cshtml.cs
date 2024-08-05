23a2442e3af894631f97a58b6ee78c260e039b93
Jonatam Sturcio <bitcomanderbr@gmail.com>
2024-08-05 15:11:43 -0300

    Finalização da parte web do modulo Funcionario

---
+++ ControleDeCinemaMVC/Views/Funcionario/detalhes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControleDeCinemaMVC.Views.Funcionario
+{
+    public class detalhesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ControleDeCinemaMVC/Views/Funcionario/editar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControleDeCinemaMVC.Views.Funcionario
+{
+    public class editarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
