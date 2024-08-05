d52f73fcbcbf24a2da8293a127af073c446a1906
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-05 15:13:31 -0300

    Revert "Terminado de Implementar os testes integ"
    This reverts commit ee377326d04d9b5bd35f0c6c456e04dab26b4d8b.

--- ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTestes.cs
+++ ControleDeCinema.Testes.Integracao/ModuloFilme/RepositorioFilmeEmOrmTestes.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using ControleDeCinema.Dominio.ModuloFilme;
-using ControleDeCinema.Infra.Orm.Compartilhado;
+﻿using ControleDeCinema.Infra.Orm.Compartilhado;
 using ControleDeCinema.Infra.Orm.ModuloFilme;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -22,73 +20,7 @@ public class RepositorioFilmeEmOrmTestes
         repositorioFilme = new RepositorioFilmeEmOrm(dbContext);
     }
 
-    [TestMethod]
-    public void Deve_Inserir_Filme_Corretamente()
-    {
-        //Arrange
-        Filme novoFilme = new Filme("", 0, "", false);
-        
-        //Act
-        repositorioFilme.Inserir(novoFilme);
-        
-        //Assert
-        Filme filmeSelecionado = repositorioFilme.SelecionarPorId(novoFilme.Id);
+    //[TestMethod]
 
-        Assert.AreEqual(novoFilme, filmeSelecionado);
-    }
-
-    [TestMethod]
-    public void Deve_Editar_Filme_Corretamente()
-    {
-        //Arrange
-        Filme filmeOriginal = new Filme("", 0, "", false);
-
-        repositorioFilme.Inserir(filmeOriginal);
-
-        Filme filmeParaAtualizacao = repositorioFilme.SelecionarPorId(filmeOriginal.Id);
-        
-        //Act
-        repositorioFilme.Editar(filmeOriginal, filmeParaAtualizacao);
-
-        //Assert
-        Assert.AreEqual(filmeOriginal, filmeParaAtualizacao);
-    }
-
-    [TestMethod]
-    public void Deve_Excluir_Filme_Corretamente()
-    {
-        //Arrange
-        Filme filme = new Filme("", 0, "", false);
-
-        repositorioFilme.Inserir(filme);
-
-        //Act
-        repositorioFilme.Excluir(filme);
-
-        //Arrange
-        Filme filmeSelecionado = repositorioFilme.SelecionarPorId(filme.Id);
-
-        Assert.IsNull(filmeSelecionado);
-    }
-
-    [TestMethod]
-    public void Deve_Selecionar_Filme_Corretamente()
-    {
-        //Arrange
-        List<Filme> filmesParaCadastro =
-        [
-            new Filme("", 0, "", false),
-            new Filme("a", 1, "a", false),
-            new Filme("b", 2, "b", true)
-        ];
-
-        foreach (Filme filme in filmesParaCadastro)
-            repositorioFilme.Inserir(filme);
-
-        //Act
-        List<Filme> filmesSelecionados = repositorioFilme.SelecionarTodos();
-
-        //Assert
-        CollectionAssert.AreEqual(filmesParaCadastro, filmesSelecionados);
-    }
 }
+// AAA - Arrange , Act, Assert
--- ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTestes.cs
+++ ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTestes.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-using ControleDeCinema.Dominio.ModuloSala;
-using ControleDeCinema.Infra.Orm.Compartilhado;
+﻿using ControleDeCinema.Infra.Orm.Compartilhado;
 using ControleDeCinema.Infra.Orm.ModuloSala;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ControleDeCinema.Testes.Integracao.ModuloSala;
 
-[TestClass]
-[TestCategory("Testes de Intregracao de Sala")]
 public class RepositorioSalaEmOrmTestes
 {
     ControleDeCinemaDbContext dbContext = null;
@@ -22,73 +18,6 @@ public class RepositorioSalaEmOrmTestes
         repositorioSala = new RepositorioSalaEmOrm(dbContext);
     }
 
-    [TestMethod]
-    public void Deve_Inserir_Sala_Corretamente()
-    {
-        //Arrange
-        Sala novaSala = new Sala(0, 0);
-
-        //Act
-        repositorioSala.Inserir(novaSala);
-
-        //Arrange
-        Sala salaSelecionada = repositorioSala.SelecionarPorId(novaSala.Id);
-
-        Assert.AreEqual(novaSala, salaSelecionada);
-    }
-
-    [TestMethod]
-    public void Deve_Editar_Sala_Corretamente()
-    {
-        //Arrange
-        Sala salaOriginal = new Sala(0, 0);
-
-        repositorioSala.Inserir(salaOriginal);
-
-        Sala salaParaAtualizacao = repositorioSala.SelecionarPorId(salaOriginal.Id);
-
-        //Act
-        repositorioSala.Editar(salaOriginal, salaParaAtualizacao);
-
-        //Assert
-        Assert.AreEqual(salaOriginal,salaParaAtualizacao);
-    }
-
-    [TestMethod]
-    public void Deve_Excluir_Sala_Corretamente()
-    {
-        //Arrange
-        Sala sala = new Sala(0, 0);
-
-        repositorioSala.Inserir(sala);
-
-        //Act
-        repositorioSala.Excluir(sala);
-
-        //Assert
-        Sala salaSelecionada = repositorioSala.SelecionarPorId(sala.Id);
-
-        Assert.IsNull(salaSelecionada);
-    }
-
-    [TestMethod]
-    public void Deve_Selecionar_Sala_Corretamente()
-    {
-        //Arrange
-        List<Sala> salaParaCadastro =
-        [
-            new Sala(0, 0),
-            new Sala(1, 1),
-            new Sala(2, 2)
-        ];
-
-        foreach(Sala sala in salaParaCadastro)
-            repositorioSala.Inserir(sala);
-
-        //Act
-        List<Sala> salaSelecionadas = repositorioSala.SelecionarTodos();
-
-        //Assert
-        CollectionAssert.AreEqual(salaParaCadastro, salaSelecionadas);
-    }
+    //[TestMethod]
 }
+// AAA - Arrange, Act, Assert
