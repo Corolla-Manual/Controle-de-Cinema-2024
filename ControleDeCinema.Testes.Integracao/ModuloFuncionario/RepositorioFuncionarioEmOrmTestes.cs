ee377326d04d9b5bd35f0c6c456e04dab26b4d8b
Eduardo <eduardotortellidudu9@uniplaclages.edu.br>
2024-08-05 15:12:36 -0300

    Terminado de Implementar os testes integ

--- ControleDeCinema.Testes.Integracao/ModuloFuncionario/RepositorioFuncionarioEmOrmTestes.cs
+++ ControleDeCinema.Testes.Integracao/ModuloFuncionario/RepositorioFuncionarioEmOrmTestes.cs
@@ -1,11 +1,14 @@
-﻿using ControleDeCinema.Infra.Orm.Compartilhado;
+﻿using System;
+using System.Collections.Generic;
+using ControleDeCinema.Dominio.ModuloFuncionario;
+using ControleDeCinema.Infra.Orm.Compartilhado;
 using ControleDeCinema.Infra.Orm.ModuloFuncionario;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ControleDeCinema.Testes.Integracao.ModuloFuncionario;
 
 [TestClass]
-[TestCategory("Testes de Integração de Funcionarios")]
+[TestCategory("Testes de Integração de Funcionario")]
 public class RepositorioFuncionarioEmOrmTestes
 {
     private ControleDeCinemaDbContext dbContext = null;
@@ -20,6 +23,73 @@ public class RepositorioFuncionarioEmOrmTestes
         repositorioFuncionario = new RepositorioFuncionarioEmOrm(dbContext);
     }
 
-    //[TestMethod]
+    [TestMethod]
+    public void Deve_Inserir_Funcionario_Corretamente()
+    {
+        //Arrange
+        Funcionario novoFuncionario = new Funcionario("", "", "");
+
+        //Act
+        repositorioFuncionario.Inserir(novoFuncionario);
+
+        //Assert
+        Funcionario funcionarioSelecionado = repositorioFuncionario.SelecionarPorId(novoFuncionario.Id);
+
+        Assert.AreEqual(novoFuncionario, funcionarioSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Funcionario_Corretamente()
+    {
+        //Arrange 
+        Funcionario funcionarioOriginal = new Funcionario("", "", "");
+
+        repositorioFuncionario.Inserir(funcionarioOriginal);
+
+        Funcionario funcionarioParaAtualizacao = repositorioFuncionario.SelecionarPorId(funcionarioOriginal.Id);
+
+        //Act
+        repositorioFuncionario.Editar(funcionarioOriginal, funcionarioParaAtualizacao);
+
+        //Assert
+        Assert.AreEqual(funcionarioOriginal, funcionarioParaAtualizacao);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Funcionario_Corretamente()
+    {
+        //Arrange
+        Funcionario funcionario = new Funcionario("", "", "");
+
+        repositorioFuncionario.Inserir(funcionario);
+
+        //Act
+        repositorioFuncionario.Excluir(funcionario);
+
+        //Assert
+        Funcionario funcionarioSelecionado = repositorioFuncionario.SelecionarPorId(funcionario.Id);
+
+        Assert.IsNull(funcionarioSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Funcionario_Corretamente()
+    {
+        //Assert
+        List<Funcionario> funcionariosParaCadastro =
+        [
+            new Funcionario("", "", ""),
+            new Funcionario("a", "a", "a"),
+            new Funcionario("b", "b", "b")
+        ];
+
+        foreach (Funcionario funcionario in funcionariosParaCadastro)
+            repositorioFuncionario.Inserir(funcionario);
+
+        //Act
+        List<Funcionario> funcionariosSelecionados = repositorioFuncionario.SelecionarTodos();
+
+        //Assert
+        CollectionAssert.AreEqual(funcionariosParaCadastro, funcionariosSelecionados);
+    }
 }
-// AAA - Arrange , Act, Assert
---
+++ ControleDeCinema.Testes.Integracao/ModuloSessao/RepositorioSessaoEmOrmTestes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Dominio.ModuloSessao;
+using ControleDeCinema.Infra.Orm.Compartilhado;
+using ControleDeCinema.Infra.Orm.ModuloSessao;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSessao;
+
+[TestClass]
+[TestCategory("Testes de Intregracao de Sessao")]
+public class RepositorioSessaoEmOrmTestes
+{
+    private ControleDeCinemaDbContext dbContext = null;
+    RepositorioSessaoEmOrm repositorioSessao = null;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = new ControleDeCinemaDbContext();
+        dbContext.Sessoes.RemoveRange(dbContext.Sessoes);
+
+        repositorioSessao = new RepositorioSessaoEmOrm(dbContext);
+    }
+
+    [TestMethod]
+    public void Deve_Inserir_Sessao_Corretamente()
+    {
+        //Arrange
+        Sala sala = new Sala(0, 0);
+        Filme filme = new Filme("", 0, "", false);
+
+        Sessao novaSessao = new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Não_Iniciado);
+
+        //Act
+        repositorioSessao.Inserir(novaSessao);
+
+        //Arrange
+        Sessao sessaoSelecionada = repositorioSessao.SelecionarPorId(novaSessao.Id);
+
+        Assert.AreEqual(novaSessao, sessaoSelecionada);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Sessao_Corretamente()
+    {
+        //Arrange
+        Sala sala = new Sala(0, 0);
+        Filme filme = new Filme("", 0, "", false);
+
+        Sessao sessaoOriginal = new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Não_Iniciado);
+
+        repositorioSessao.Inserir(sessaoOriginal);
+
+        Sessao sessaoParaAtualizacao = repositorioSessao.SelecionarPorId(sessaoOriginal.Id);
+
+        //Act
+        repositorioSessao.Editar(sessaoOriginal, sessaoParaAtualizacao);
+
+        //Assert 
+        Assert.AreEqual(sessaoOriginal, sessaoParaAtualizacao);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Sessao_Corretamente()
+    {
+        //Arrange 
+        Sala sala = new Sala(0, 0);
+        Filme filme = new Filme("", 0, "", false);
+
+        Sessao sessao = new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Não_Iniciado);
+
+        repositorioSessao.Inserir(sessao);
+
+        //Act
+        repositorioSessao.Excluir(sessao);
+
+        //Assert
+        Sessao sessaoSelecionada = repositorioSessao.SelecionarPorId(sessao.Id);
+
+        Assert.IsNull(sessaoSelecionada);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Sessao_Corretamente()
+    {
+        Sala sala = new Sala(0, 0);
+        Filme filme = new Filme("", 0, "", false);
+
+        //Arrange
+        List<Sessao> sessoesParaCadastro =
+        [
+            new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Não_Iniciado),
+            new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Em_Andamento),
+            new Sessao(filme, sala, DateTime.MinValue, StatusSessaoEnum.Finalizado)
+        ];
+
+        foreach(Sessao sessao in sessoesParaCadastro)
+            repositorioSessao.Inserir(sessao);
+
+        //Act
+        List<Sessao> sessoesSelecionadas = repositorioSessao.SelecionarTodos();
+
+        //Assert
+        CollectionAssert.AreEqual(sessoesParaCadastro, sessoesSelecionadas);
+    }
+}
+
